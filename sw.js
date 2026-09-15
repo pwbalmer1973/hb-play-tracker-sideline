e5f98a866f092e34d2f79c75f6d5567b41c059a7
pwbalmer1973 <paul.balmer@gmail.com>
2026-09-15 08:36:17 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// HB Play Tracker Sideline - offline service worker
+// HB Sideline Play Tracker - offline service worker
 // IMPORTANT: bump the version below every time you upload a new index.html,
 // otherwise devices keep serving the old cached copy.
-var CACHE = 'hb-sideline-v6';
+var CACHE = 'hb-sideline-v8';
 
 var ASSETS = [
   './',
